8ff0f7990da9c87a073c7e43a53b2f1644ac4e76
Kacper Rzońca <kacper11812@gmail.com>
2026-07-19 19:39:42 +0200

    design mining view

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import PlayerProfile
+from .game_logic import start_mining, claim_ore_reward
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def mine_view(request):
+    profile = PlayerProfile.objects.get(id=1)
+    if request.method == "POST":
+        action = request.POST.get("action")
+        if action == "mine":
+            start_mining(profile, 15)
+            return redirect(request.path)
+        elif action == "claim":
+            claim_ore_reward(profile)
+            return redirect(request.path)
+    return render(request, "characters/mine.html", {"profile": profile})
