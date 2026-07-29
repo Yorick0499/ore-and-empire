9b48925b6a450df4e9e5fa541482ad4e8be498df
Kacper Rzońca <kacper11812@gmail.com>
2026-07-29 19:05:41 +0200

    register item blue print and owned item models

--- characters/admin.py
+++ characters/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import PlayerProfile
+from .models import ItemBluePrint, OwnedItem
+
 
 admin.site.register(PlayerProfile)
+admin.site.register(ItemBluePrint)
+admin.site.register(OwnedItem)
